01068c9dd80c34d3c65ac3fc014a9d74bc039b51
QIYUEKURONG <605753304@qq.com>
2019-01-01 16:04:01 +0800

    test

---
+++ daemod.c
@@ -0,0 +1,65 @@
+﻿
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/param.h>
+#incldue<time.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+void init_daemon()
+{
+	pid_t pid;
+	pid=fork();
+	if(pid>0)
+	{
+		exit(0);
+	}else if(pid<0)
+	{
+		exit(1);
+	}
+	else
+	{
+		setsid();
+		pid=fork();
+		if(pid>0)
+		{
+			exit(0);
+		}
+		else if(pif<0)
+		{
+			exit(1);
+		}
+		else
+		{
+			int i;
+			for(i=0;i<NOFILE;++i)
+			{
+				close(i);
+			}
+			chdir("/");
+			umask(0);
+		}
+	
+	}
+	return ;
+}
+void main()
+{
+	FILE *fp;
+	time_t t;
+	fp=open("test.log","a");
+	init_daemon();
+	while(1)
+	{
+		sleep(5);
+		if(fp>=0)
+		{
+			time(&t);
+			fprintf(fp,"current time is:%s\n",asctime(localtime(&t)))   
+			fclose(fp);
+		}
+	}
+	return ;
+}
+
